ccdfe113a470e3d9bf6407132e909ff4b4c707ac
Ashutosh Naik <naikashutosh.67@gmail.com>
2024-04-20 17:42:01 -0600

    add trailingSlash in layout.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { base } from "$app/paths";
     import { onMount } from "svelte";
     import "../app.css"
     import Navbar from "$lib/components/Navbar.svelte";
@@ -32,7 +31,7 @@
     </svelte:fragment>
     <svelte:fragment slot="trailing">
         <ul class="menu menu-horizontal px-1">
-        <li><a class="font-semibold" href={base + "/resume.pdf"} target="_blank">resume</a></li>
+        <li><a class="font-semibold" href="/resume.pdf" target="_blank">resume</a></li>
         </ul>
     </svelte:fragment>
 </Navbar>
